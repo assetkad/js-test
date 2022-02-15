1140b7d0a98a123d0bcadd808862f4a8a6bd4a33
Asset <asset_kad@mail.ru>
2022-02-15 12:36:19 +0500

    lol

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let question = confirm(`Is everything with your life good?`);
+if (question == true) {
+    alert(`It's nice`);
+} else {
+    alert(`What happend with your life?`)
+}
